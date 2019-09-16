fd787e50506d2216520b346e3145bad7cd2f11fa
George Yord <george.yord@gmail.com>
2019-09-16 14:30:50 +0300

    :sparkles: Add new remoteMethod 'isPrivate' for model Room

--- common/models/room.js
+++ common/models/room.js
@@ -1,5 +1,16 @@
 'use strict';
 
 module.exports = function(Room) {
-
+  Room.remoteMethod(
+    'isPrivate', {
+      http: {
+        path: '/isPrivate',
+        verb: 'get',
+      },
+      returns: {
+        arg: 'isPrivate',
+        type: 'boolean',
+      },
+    }
+  );
 };
