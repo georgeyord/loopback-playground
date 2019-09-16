23420d90cd631ef38baa29e120c2f5d96688b806
George Yord <george.yord@gmail.com>
2019-09-16 14:14:24 +0300

    :rotating_light: Fix linting issues

--- common/models/message.js
+++ common/models/message.js
@@ -1,7 +1,4 @@
-// Copyright IBM Corp. 2016. All Rights Reserved.
-// Node module: loopback-workspace
-// This file is licensed under the MIT License.
-// License text available at https://opensource.org/licenses/MIT
+'use strict';
 
 module.exports = function(Message) {
   Message.greet = function(msg, cb) {
