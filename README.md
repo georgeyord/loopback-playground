ec3581942cd8debf6b3f974f82bb731cbf114812
George Yord <george.yord@gmail.com>
2019-09-16 17:37:30 +0300

    Init db data on boot

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # loopback-playground
 Playing around with loopback.io
+
+# Next steps
+
+- Read migration samples from json
+- Use async
---
+++ server/boot/migration.js
@@ -0,0 +1,50 @@
+'use strict';
+const Promise = require('bluebird');
+
+module.exports = function(app) {
+  var cardSets = [
+    {
+      'name': 'Fibonacci',
+      'values': [
+        '1',
+        '2',
+        '3',
+        '5',
+        '8',
+        '13',
+        '21',
+        '34',
+        '55',
+        '89',
+        '&#9749;',
+        '&#F937;',
+        '?',
+      ],
+    },
+    {
+      'name': 'T-shirt Size',
+      'values': [
+        'XXS',
+        'XS',
+        'S',
+        'M',
+        'L',
+        'XL',
+        'XXL',
+        '?',
+      ],
+    },
+  ];
+
+  Promise.map(cardSets, ({name, values}) =>
+    app.models.CardSet.findOrCreate({where: {name}}, {name})
+    .then(([{id}]) =>
+      Promise.map(values, (value) =>
+        app.models.Card.findOrCreate(
+          {where: {value: value, cardSetID: id}},
+          {value: value, cardSetID: id}
+        )
+      )
+    )
+  );
+};
