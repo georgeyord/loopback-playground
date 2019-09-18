4c06f16c49626659fff17ba966d2d2383bdf57e7
George Yord <george.yord@gmail.com>
2019-09-18 17:23:14 +0300

    :card_file_box: Read migration data from json files

--- server/boot/migration.js
+++ server/boot/migration.js
@@ -1,40 +1,8 @@
 'use strict';
 
-module.exports = function (app) {
-  const cardSets = [
-    {
-      name: 'Fibonacci',
-      values: [
-        '1',
-        '2',
-        '3',
-        '5',
-        '8',
-        '13',
-        '21',
-        '34',
-        '55',
-        '89',
-        '&#9749;',
-        '&#F937;',
-        '?'
-      ]
-    },
-    {
-      name: 'T-shirt Size',
-      values: [
-        'XXS',
-        'XS',
-        'S',
-        'M',
-        'L',
-        'XL',
-        'XXL',
-        '?'
-      ]
-    }
-  ];
+const cardSets = require('./migrations/cardsets.json');
 
+module.exports = function (app) {
   Promise.map(cardSets, ({ name, values }) => app.models.CardSet.findOrCreate(
     { where: { name } },
     { name }
